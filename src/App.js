a1eb3964c1560c8ba6c52a7a9e18fa3a7a2e87e8
Nicolas <invisiman042@gmail.com>
2022-10-10 17:35:23 +0200

    section2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { contentBlock1, contentBlock2, contentBlock3 } from "./content.js";
+
 function App() {
-  return <Section1 />;
+  return (
+    <div className="app">
+      <Section1 />
+      <Section2 title={"[ma·routine] visage essentielle"} />
+    </div>
+  );
 }
 
 export default App;
@@ -25,4 +32,41 @@ function Button({ name, text }) {
       <button className={`${name}-button`}>{text}</button>
     </div>
   );
+}
+
+function Section2({ title }) {
+  return (
+    <div className="section2">
+      <div className="section2-title">{title}</div>
+      <div className="block-container">
+        <Block id={1} content={contentBlock1} />
+        <Block id={2} content={contentBlock2} />
+        <Block id={3} content={contentBlock3} />
+      </div>
+    </div>
+  );
+}
+
+function Block({ content, id }) {
+  return (
+    <div className={`block${id}`}>
+      <div className="block-title">{content.title}</div>
+      <div className="block-intro">{content.intro}</div>
+      <List content={content.list} />
+    </div>
+  );
+}
+
+function List({ content }) {
+  let renderedList = content.map((element, index) => {
+    return (
+      <div className="list-content">
+        <span>{"⚡️"}</span>
+        <div className="list-text" key={index}>
+          {element}
+        </div>
+      </div>
+    );
+  });
+  return <div className="block-list">{renderedList}</div>;
 }
---
+++ src/content.js
@@ -0,0 +1,55 @@
+const titleBlock1 = "Nettoyer";
+const introBlock1 = `Le stick 2en1 nettoyant – démaquillant se transforme en lait au 
+    contact de l’eau, éliminant les impuretés et le maquillage en douceur. 
+    Son pH est adapté à votre peau afin de respecter son équilibre.`;
+const listBlock1 = [
+  `Le decyl glucoside d’origine végétale nettoie les peaux les plus sensibles.`,
+  `Les algues rouges luttent contre les agressions de la pollution.`,
+  `L’extrait de pamplemousse rose riche en vitamine C antioxydant et tonifiant.`,
+];
+const colorBlock1 = "#d9f6fa";
+
+const contentBlock1 = {
+  title: titleBlock1,
+  intro: introBlock1,
+  list: listBlock1,
+  color: colorBlock1,
+};
+// ---------------------------------------------------------------------------------
+const titleBlock2 = "Nourir";
+const introBlock2 = `La crème est un soin du quotidien. Sa texture pénètre sans 
+laisser de fini collant. Elle nourrit, préserve l’élasticité pour une peau plus 
+douce et repulpée. Riche en bêta-carotène pour un teint bonne mine et éclatant .`;
+const listBlock2 = [
+  `La Vitamine F maintient la barrière hydro-lipidique en bonne santé.`,
+  `Le beurre de karité nourrit et préserve l’élasticité de la peau.`,
+  `L’extrait de carotte et huile de noyau d’abricot ravivent l’éclat du teint.`,
+];
+const colorBlock2 = "#fdd2bb";
+
+const contentBlock2 = {
+  title: titleBlock2,
+  intro: introBlock2,
+  list: listBlock2,
+  color: colorBlock2,
+};
+// ---------------------------------------------------------------------------------
+const titleBlock3 = "Exfolier";
+const introBlock3 = `Le masque peeling aux acides de fruits (A.H.A) exfolie en 
+douceur les cellules mortes favorisant la régénération de la peau. Boosté par 
+l’extrait de pamplemousse rose et l’argile rose pour un teint détoxifié, lisse et éclatant.`;
+const listBlock3 = [
+  `Les A.H.A (pêche, pomme et framboise) atténuent les irrégularités et uniformisent le teint.`,
+  `L’argile rose purifie et redonne de l’éclat au teint.`,
+  `L’extrait de pamplemousse rose riche en Vitamine C antioxydant et tonifiant.`,
+];
+const colorBlock3 = "#d7f6e7";
+
+const contentBlock3 = {
+  title: titleBlock3,
+  intro: introBlock3,
+  list: listBlock3,
+  color: colorBlock3,
+};
+
+export { contentBlock1, contentBlock2, contentBlock3 };
